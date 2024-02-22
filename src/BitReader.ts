47ea8d5b158f5048908a948913180e4e7331a6f6
Thomas Jammet <jammetthomas@gmail.com>
2024-02-22 20:35:20 +0100

    style(copyright): update copyright year

--- src/BitReader.ts
+++ src/BitReader.ts
@@ -1,9 +1,12 @@
 /**
- * Copyright 2023 Ceeblue B.V.
+ * Copyright 2024 Ceeblue B.V.
  * This file is part of https://github.com/CeeblueTV/web-utils which is released under GNU Affero General Public License.
  * See file LICENSE or go to https://spdx.org/licenses/AGPL-3.0-or-later.html for full license details.
  */
 
+/**
+ * BitReader allows to read binary data bit by bit
+ */
 export class BitReader {
     private _data: Uint8Array;
     private _size: number;
