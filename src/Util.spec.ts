38e0e21bab50168eba95313fe288d94271cb44c4
MathieuPoux <mathieu.poux@ceeblue.net>
2025-04-15 10:54:18 +0200

    refactor(Util.ts): test and fix time feature into Util.ts
    BREAKING CHANGE: Remove useless Util.timeOrigin() function

---
+++ src/Util.spec.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2024 Ceeblue B.V.
+ * This file is part of https://github.com/CeeblueTV/web-utils which is released under GNU Affero General Public License.
+ * See file LICENSE or go to https://spdx.org/licenses/AGPL-3.0-or-later.html for full license details.
+ */
+
+import { describe, it, expect } from 'vitest';
+import * as Util from './Util';
+
+describe('Util', () => {
+    describe('time functions', () => {
+        it('should calculate passed time', async () => {
+            const time0 = Util.time();
+            await new Promise(resolve => setTimeout(resolve, 100));
+            const time100 = Util.time();
+            expect(time100).toBeGreaterThanOrEqual(time0 + 100);
+
+            const unixTime = Util.unixTime();
+            expect(unixTime).toBeGreaterThanOrEqual(performance.timeOrigin + time0 + 100);
+            expect(unixTime).toBeGreaterThanOrEqual(performance.timeOrigin + time100);
+        });
+    });
+});
