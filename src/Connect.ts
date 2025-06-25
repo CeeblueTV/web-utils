576593c47082ab2ebee733d808f5df9fd5118d17
MathieuPoux <mathieu.poux@ceeblue.net>
2025-06-25 11:57:18 +0200

    docs(Connect.ts): fix a JS comment

--- src/Connect.ts
+++ src/Connect.ts
@@ -113,7 +113,7 @@ export function defineMediaExt(type: Type, params: Params) {
             // Set mediaExt with ?ext= param when set OR url extension
             params.mediaExt = url.searchParams.get('ext') ?? Util.getExtension(Util.getFile(url.pathname));
         } catch (_) {
-            // not an URL, it's only a host => keep mediaExt unchanged to build the URL
+            // not an URL, it's only a host
             params.mediaExt = '';
         }
     }
